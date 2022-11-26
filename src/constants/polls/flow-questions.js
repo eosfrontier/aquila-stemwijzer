fe71c345bbf8b9fda99d3753ca4a0500ec78351c
Thijs Boerma <th.boerma@gmail.com>
2022-11-26 21:09:52 +0100

    Translations by Mai (tyvm!!)

--- src/constants/polls/flow-questions.js
+++ src/constants/polls/flow-questions.js
@@ -1,37 +1,55 @@
-import {QuestionIds} from './enums';
+import { QuestionIds } from './enums';
 
 export const POLL_QUESTIONS = {
-  [QuestionIds.DefenseIsolate]:
-      'De Republiek moet zijn defensieapparaat puur inzetten ter verdediging van de grenzen van de Republiek.',
-  [QuestionIds.DefensePrivatise]:
-      'Defensie moet meer van zijn eenheden openstellen voor activiteiten van private partijen.',
-  [QuestionIds.DefenseXenoWarEffort]:
-      'De Republiek moet zijn militaire kracht zo ver als mogelijk is aan het ICC uitbesteden om de dreiging van het vijandige ras te neutraliseren.',
-  [QuestionIds.FinanceImportTax]:
-      'Er moeten extra invoerheffingen op producten die buiten de Republiek zijn geproduceerd komen.',
-  [QuestionIds.InterFacLeadingRole]: 'De Republiek moet een voortrekkersrol spelen binnen het ICC',
-  [QuestionIds.InterFacCommand]:
-      'De Republiek moet de leiding nemen in het proces om de dreiging van het onbekende ras weg te nemen.',
-  [QuestionIds.InterFacOurValues]:
-      'De Republiek moet zijn normen en waarden meer uit gaan dragen richting het ICC en de andere facties.',
-  [QuestionIds.GovGlobalInfluence]:
-      'De overheid/centrale regering moet een grotere rol hebben in het dagelijks leven van de Aquilaan.',
-  [QuestionIds.GovPlanetaryInfluence]:
-      'De planetaire regeringen moeten meer mandaat van de centrale regering krijgen.',
-  [QuestionIds.PortalInfrastructure]:
-      'De Republiek moet meer energie stoppen in het ontwikkelen en implementeren van het Republieksbrede portaalnetwerk.',
-  [QuestionIds.GovOwnedCorps]:
-      'De centrale overheid mag geen staatsbedrijven meer in bezit hebben. ',
-  [QuestionIds.CultureEducateExternals]:
-      'Het is belangrijk om niet-Aquilanen kennis te laten maken met onze cultuur en geschiedenis.',
-  [QuestionIds.CultureOpenWorldPolicy]:
-      'De Republiek moet het makkelijker maken om niet-Aquilanen op planeten van de Republiek te laten vestigen.',
-  [QuestionIds.CultureIntegrateExternals]:
-      'De middelen en mogelijkheden voor niet-Aquilanen om het Aquilaanse burgerschap te verkrijgen moeten een herinrichting krijgen met als doel de versoepeling van het proces. ',
-  [QuestionIds.CultureScienceLead]:
-      'De Republiek moet toonaangevend zijn op het gebied van wetenschappelijk onderzoek.',
-  [QuestionIds.FinanceEducationCosts]:
-      'De Republiek moet eigen bijdrage voor opleidingen die door de Republiek worden aangeboden verlagen.',
-  [QuestionIds.GovMairsInfluence]:
-      'Bij het maken van politieke beslissingen moeten de lessen van Mair meegewogen worden.',
+    [QuestionIds.DefenseIsolate]:
+        'The Republic must use its defenses only for defending the borders of the Republic.',
+    // 'De Republiek moet zijn defensieapparaat puur inzetten ter verdediging van de grenzen van de Republiek.',
+    [QuestionIds.DefensePrivatise]:
+        'The Ministry of Defence must apply more of its forces for the activities of private instances.',
+    // 'Defensie moet meer van zijn eenheden openstellen voor activiteiten van private partijen.',
+    [QuestionIds.DefenseXenoWarEffort]:
+        'The Republic must lend its military forces to the ICC as much as possible to neutralize the threat of the enemy/hostile race.',
+    //   'De Republiek moet zijn militaire kracht zo ver als mogelijk is aan het ICC uitbesteden om de dreiging van het vijandige ras te neutraliseren.',
+    [QuestionIds.FinanceImportTax]:
+        'Extra import fees should be applied to products produced outside of the Republic.',
+    //   'Er moeten extra invoerheffingen op producten die buiten de Republiek zijn geproduceerd komen.',
+    [QuestionIds.InterFacLeadingRole]:
+        'The Republic should have a leading role within the ICC.',
+    // 'De Republiek moet een voortrekkersrol spelen binnen het ICC',
+    [QuestionIds.InterFacCommand]:
+        'The Republic must take the lead in the process of removing the threat of the unknown race.',
+    //   'De Republiek moet de leiding nemen in het proces om de dreiging van het onbekende ras weg te nemen.',
+    [QuestionIds.InterFacOurValues]:
+        'The Republic must propagate their values and ideals more to the ICC and other factions.',
+    //   'De Republiek moet zijn normen en waarden meer uit gaan dragen richting het ICC en de andere facties.',
+    [QuestionIds.GovGlobalInfluence]:
+        'The government should play a bigger role in the daily life of an Aquilan Citizen.',
+    //   'De overheid/centrale regering moet een grotere rol hebben in het dagelijks leven van de Aquilaan.',
+    [QuestionIds.GovPlanetaryInfluence]:
+        'The planetary governments should receive an extension of their mandate from the central government.',
+    //   'De planetaire regeringen moeten meer mandaat van de centrale regering krijgen.',
+    [QuestionIds.PortalInfrastructure]:
+        'The Republic must focus their efforts in developing and implementing the Republic wide portal network.',
+    //   'De Republiek moet meer energie stoppen in het ontwikkelen en implementeren van het Republieksbrede portaalnetwerk.',
+    [QuestionIds.GovOwnedCorps]:
+        'The central government is not allowed to control any state owned companies.',
+    // 'De centrale overheid mag geen staatsbedrijven meer in bezit hebben. ',
+    [QuestionIds.CultureEducateExternals]:
+        'It is important for non-Aquilans to know about our culture and history.',
+    //   'Het is belangrijk om niet-Aquilanen kennis te laten maken met onze cultuur en geschiedenis.',
+    [QuestionIds.CultureOpenWorldPolicy]:
+        'The Republic must make it easier for non-Aquilans to settle on the planets of the Republic.',
+    // 'De Republiek moet het makkelijker maken om niet-Aquilanen op planeten van de Republiek te laten vestigen.',
+    [QuestionIds.CultureIntegrateExternals]:
+        'The means and opportunities for non-Aquilans to achieve Aquilan citizenship should be restructured with the goal of making the process easier.',
+    // 'De middelen en mogelijkheden voor niet-Aquilanen om het Aquilaanse burgerschap te verkrijgen moeten een herinrichting krijgen met als doel de versoepeling van het proces. ',
+    [QuestionIds.CultureScienceLead]:
+        'The Republic must be a prominent figure in the area of scientific research.',
+    // 'De Republiek moet toonaangevend zijn op het gebied van wetenschappelijk onderzoek.',
+    [QuestionIds.FinanceEducationCosts]:
+        'The Republic must lower the supplementary tuition fees for education offered by the Republic.',
+    // 'De Republiek moet eigen bijdrage voor opleidingen die door de Republiek worden aangeboden verlagen.',
+    [QuestionIds.GovMairsInfluence]:
+        'The lessons of Maïr must be taken into consideration while making decisions of a political nature.'
+    // 'Bij het maken van politieke beslissingen moeten de lessen van Mair meegewogen worden.',
 };
